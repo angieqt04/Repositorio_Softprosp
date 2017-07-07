37d162af67cbfc44b6693089cf2b8cb0ea54024e
angieqt <angieqt04@gmail.com>
2017-07-07 17:09:09 -0500

    Formularios p1

--- Softprosp/urls.py
+++ Softprosp/urls.py
@@ -1,12 +1,13 @@
-
 from django.conf.urls import patterns,include, url
 from django.contrib import admin
-from Benchmarking.views import index
+
+from core_apps.cuantitativas.Benchmarking import index
+
 admin.autodiscover()
 
 
 urlpatterns = patterns('',
-    url(r'^$', index, name='index'),
-    url(r'^admin/', include(admin.site.urls)),
-    url(r'^benchmarking/', include('Benchmarking.urls', namespace="benchmarking")),
-)
+                       url(r'^$', index, name='index'),
+                       url(r'^admin/', include(admin.site.urls)),
+                       url(r'^benchmarking/', include('core_apps.cuantitativas.Benchmarking.urls', namespace="benchmarking")),
+                       )
