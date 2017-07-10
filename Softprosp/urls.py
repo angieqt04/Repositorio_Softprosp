9bcaa08393604009f0bc23a2b0fb5032035e02f1
angieqt <angieqt04@gmail.com>
2017-07-10 16:15:38 -0500

    Ultima version

--- Softprosp/urls.py
+++ Softprosp/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls import patterns,include, url
 from django.contrib import admin
 
-from core_apps.cuantitativas.Benchmarking import index
+from Benchmarking import index
 
 admin.autodiscover()
 
@@ -9,5 +9,5 @@ admin.autodiscover()
 urlpatterns = patterns('',
                        url(r'^$', index, name='index'),
                        url(r'^admin/', include(admin.site.urls)),
-                       url(r'^benchmarking/', include('core_apps.cuantitativas.Benchmarking.urls', namespace="benchmarking")),
+                       url(r'^benchmarking/', include('Benchmarking.urls', namespace="benchmarking")),
                        )
